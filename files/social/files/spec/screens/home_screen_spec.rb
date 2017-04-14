c0934a0046ce6ba3789830d6dc5460d0971961a6
Liu Lantao <liulantao@gmail.com>
2017-04-14 14:55:41 +0700

    ProMotion
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

---
+++ files/social/files/spec/screens/home_screen_spec.rb
@@ -0,0 +1,39 @@
+describe HomeScreen do
+  tests HomeScreen
+
+  before { UIView.setAnimationsEnabled false }
+
+  def home_screen
+    @home_screen ||= HomeScreen.new(nav_bar: true).tap {|home_screen| home_screen.on_load }
+  end
+
+  def controller
+    home_screen.navigationController
+  end
+
+  after { @home_screen = nil }
+
+  it "has a navigationController" do
+    home_screen.navigationController.should.be.kind_of(UINavigationController)
+  end
+
+  it "has a right nav bar button" do
+    home_screen.navigationItem.rightBarButtonItem.should.be.kind_of(UIBarButtonItem)
+  end
+
+  it "has right nav bar buttons" do
+    home_screen.navigationItem.rightBarButtonItems.size.should >= 1
+    home_screen.navigationItem.rightBarButtonItems.each do |item|
+      item.should.be.kind_of(UIBarButtonItem)
+    end
+  end
+
+  it "opens the help screen when tapping Help" do
+    3.times { tap("Help") }
+
+    wait 0.1 do
+      controller.topViewController.should.be.kind_of(HelpScreen)
+    end
+  end
+
+end
